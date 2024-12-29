e9d1426515995a59c6380fe53907edf23d142c17
khenfer salah eddine <107678289+salahkhenfer@users.noreply.github.com>
2024-12-29 02:11:40 +0100

    refactor image and font loading logic in App component; streamline promises for better readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-/* import { Outlet } from "react-router-dom"; */
+import { Outlet } from "react-router-dom";
 import Logo from "../src/assets/Logo.png";
 import heroImg1 from "../src/assets/landingPage/hero/heroImg1.png";
 import heroImg2 from "../src/assets/landingPage/hero/heroImg2.png";
@@ -10,83 +10,64 @@ import studentImg from "../src/assets/landingPage/StatisticsSection/studentImg.p
 import teacher from "../src/assets/landingPage/StatisticsSection/teacher.png";
 
 import "./index.css";
-import { Outlet } from "react-router-dom";
 import MainLoading from "./MainLoading";
-// import LendingPage from "./LandingPage/LandingPage";
 
 function App() {
   const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    const fetch_images = () => {
-      return new Promise((resolve, reject) => {
-        const images = [
-          Logo,
-          heroImg1,
-          heroImg2,
-          heroImg3,
-          heroImg4,
-          starImg,
-          studentImg,
-          teacher,
-        ];
+    const fetchImages = () => {
+      const images = [
+        Logo,
+        heroImg1,
+        heroImg2,
+        heroImg3,
+        heroImg4,
+        starImg,
+        studentImg,
+        teacher,
+      ];
 
-        images.forEach((imageSrc) => {
-          const img = new Image();
-          img.onload = () => {
-            resolve();
-          };
-          img.onerror = () => {
-            resolve();
-          };
-          img.src = imageSrc;
-        });
-      });
+      return Promise.all(
+        images.map((imageSrc) => {
+          return new Promise((resolve) => {
+            const img = new Image();
+            img.onload = resolve;
+            img.onerror = resolve;
+            img.src = imageSrc;
+          });
+        })
+      );
     };
 
-    const fetch_fonts = () => {
-      return new Promise((resolve, reject) => {
-        const fontURL =
-          "https://fonts.googleapis.com/css2?family=Readex+Pro:wght@160..700&display=swap";
-        const loadFont = (url) => {
-          return new Promise((resolve, reject) => {
-            const link = document.createElement("link");
-            link.href = url;
-            link.rel = "stylesheet";
-            link.onload = () => {
-              resolve(); // Resolve promise when font is loaded
-            };
-            link.onerror = () => {
-              document.getElementById("root").style.fontFamily = "sans-serif";
-              resolve(); // Resolve even if font fails to load
-            };
-            document.head.appendChild(link);
-            document.getElementById("root").style.fontFamily = "Readex Pro";
-          });
+    const fetchFont = () => {
+      const fontURL =
+        "https://fonts.googleapis.com/css2?family=Readex+Pro:wght@160..700&display=swap";
+
+      return new Promise((resolve) => {
+        const link = document.createElement("link");
+        link.href = fontURL;
+        link.rel = "stylesheet";
+        link.onload = () => {
+          document.getElementById("root").style.fontFamily = "Readex Pro";
+          resolve();
         };
-
-        // Load the font
-        loadFont(fontURL)
-          .then(resolve)
-          .catch(() => {
-            document.getElementById("root").style.fontFamily = "sans-serif";
-            resolve();
-          });
+        link.onerror = () => {
+          document.getElementById("root").style.fontFamily = "sans-serif";
+          resolve();
+        };
+        document.head.appendChild(link);
       });
     };
 
-    Promise.all([fetch_fonts(), fetch_images()])
-      .then(() => {
-        setLoading(false);
-      })
-      .catch(() => {
-        setLoading(false);
-      });
+    Promise.all([fetchImages(), fetchFont()])
+      .then(() => setLoading(false))
+      .catch(() => setLoading(false));
   }, []);
 
   return (
     <div>
       {loading && <MainLoading />}
-
       {!loading && <Outlet />}
     </div>
   );
